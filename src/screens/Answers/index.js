89c60b2173dca33361453b2b07063f4cf67aa4a5
Krsnananda <krsnananda@visie.com.br>
2021-11-12 18:52:32 -0300

    Refina exibição de resultados

--- src/screens/Answers/index.js
+++ src/screens/Answers/index.js
@@ -11,13 +11,14 @@ import { Description, Item, QuestionList, QuestionWrapper } from "./styles"
  * Components
  * ========
  */
-import { List, Caption } from "react-native-paper"
+import { List, Caption, Divider, Colors } from "react-native-paper"
 /**
  * ========
  * Utils
  * ========
  */
 import { decode } from "html-entities"
+import { View } from "react-native"
 
 export default function Answers({ route }) {
   const answers = route.params.answers
@@ -29,13 +30,20 @@ export default function Answers({ route }) {
         <QuestionList>
           <List.Section>
             {list.map((item, index) => (
-              <Item key={index}>
-                <Description>{decode(item.question)}</Description>
-                <Caption>Right answer: {item.correct_answer}</Caption>
-                {answers.map((unique, i) => (
-                  <Caption key={i} >{unique.quest == index && unique.answer}</Caption>
-                ))}
-              </Item>
+              <View key={index}>
+                <Item>
+                  <Description>{decode(item.question)}</Description>
+                  <Caption>Right answer: {item.correct_answer}</Caption>
+                  {answers.map((unique, i) => (
+                    unique.quest == index && (
+                      <Caption key={i} style={{ color: unique.answer == 'True' ? Colors.green200 : Colors.red200 }}>
+                        {'Player: ' + unique.answer}
+                      </Caption>
+                    )
+                  ))}
+                </Item>
+                <Divider />
+              </View>
             ))}
           </List.Section>
         </QuestionList>
